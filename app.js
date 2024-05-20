5f4b152ddf4d0c744872fbe7e620b61f5516c67a
Ashutosh Golwalkar <ashutosh.golwalkar@team.telstra.com>
2024-05-20 16:36:47 +0530

    Commiting 1st iteration

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 const express = require("express");
 const fileUpload = require('express-fileupload');
-const cors = require('cors');
 const path = require('path');
-const { hostname, networkInterfaces } = require('os');
+const cors = require('cors');
 const { uploadController, listController } = require("./src/controllers");
 const middleware = require('./src/middlewares');
 const logger = require("./src/utils/logger");
 require('dotenv').config();
 
 const app = express();
-const PORT = 4000;
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(fileUpload());
+app.use(cors());
 
-
-
-app.use('/api', (req, res, next) => {
-    console.log("THIS IS GETTING CALLED");
-    console.log(req)
-    next();
-})
 //Adding Controller Routes
 uploadController(app, middleware);
 listController(app);
@@ -32,11 +23,22 @@ app.get('/style.css', function(req, res) {
     res.sendFile(path.join(__dirname, 'public', "style.css"));
 });
 
-//Rendering HTML
-app.get("/home", (req, res) => {
-    logger.info(`App - Rendering HTML`)
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/home', (req, res) => {
+    res.render("index.ejs", {
+        rootIP: process.env.ROOT_IPV4,
+        port: process.env.PORT
+    });
 });
+
+//Rendering HTML
+// app.get("/home", (req, res) => {
+//     logger.info(`App - Rendering HTML`)
+//     const options = {
+//         root: path.join(__dirname),
+//         rootIP: process.env.ROOT_IPV4
+//     }
+//     res.sendFile(path.join('public', 'index.html'), options);
+// });
 
 
 app.get('/', (req,res) => {
